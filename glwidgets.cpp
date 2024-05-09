Swyter/openbrf-redux

commit 3813f1ed025d22ecefda2fb442954b23553422ee

glwidgets, bindTexturePatch: Get the OpenGL extensions supported by the graphics card, and conditionally enable anisotropic filtering with the maximum amount of taps for a less blurry viewport.

glwidgets.cpp
@@ -1684,6 +1684,19 @@ void GLWidget::initOpenGL2(){
     //glewInit();
 	//initFramPrograms();
     qDebug("Init glew2!");
+
+	/* swy: retrieve the list of supported OpenGL extensions; first as a long space-separated string,
+	        then split into a QtSet dictionary for fast lookups */
+	const char *thing = (const char *) glGetString(GL_EXTENSIONS);
+	supportedExtensionsList = QString(thing).split(" ").toSet();
+	qDebug() << supportedExtensionsList;
+
+	if (supportedExtensionsList.contains("GL_EXT_texture_filter_anisotropic"))
+	{
+		glGetIntegerv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, (GLint *) &maxSupportedTexAnisoTaps);
+		qDebug("swy: GL_EXT_texture_filter_anisotropic is supported, with x%u taps...", maxSupportedTexAnisoTaps);
+	}
+
 	openGL2ready = true;
 
 	readCustomShaders();

glwidgets.h
@@ -74,6 +74,9 @@ public:
 	MaterialError lastMatErr;
 	void setMaterialError(int newErr);
 
+	QSet<QString> supportedExtensionsList;
+	uint8_t maxSupportedTexAnisoTaps = 0;
+
 	void keyPressEvent( QKeyEvent * event );
 	void keyReleaseEvent( QKeyEvent * event );
 private slots:

bindTexturePatch.h
@@ -194,6 +194,9 @@ bool GLWidget::myBindTexture(const QString &fileName, DdsData &data)
     } else
       glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
+    if (GLWidget::maxSupportedTexAnisoTaps > 1)
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, GLWidget::maxSupportedTexAnisoTaps);
+
     int offset = 0;
     int w = ddsHeader.dwWidth;
     int h = ddsHeader.dwHeight;

commit 004371ee653d6c79d67a8eb83ca1aa480b223fec

glwidgets: Fix the 3D viewport model appearing white and buggy on startup. This was caused by overflowing the uint8_t on the class structure and setting the following fields that contain the render modes to zero. Don't cast the write and stop trying to be smart by using smaller types to my own detriment. Bug introduced in 3813f1ed0.

glwidgets.cpp
@@ -1692,7 +1692,7 @@ void GLWidget::initOpenGL2(){
 
 	if (supportedExtensionsList.contains("GL_EXT_texture_filter_anisotropic"))
 	{
-		glGetIntegerv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, (GLint *) &maxSupportedTexAnisoTaps);
+		glGetIntegerv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &maxSupportedTexAnisoTaps);
 		qDebug("swy: GL_EXT_texture_filter_anisotropic is supported, with x%u taps...", maxSupportedTexAnisoTaps);
 	}
 

glwidgets.h
@@ -75,7 +75,7 @@ public:
 	void setMaterialError(int newErr);
 
 	QSet<QString> supportedExtensionsList;
-	uint8_t maxSupportedTexAnisoTaps = 0;
+	int32_t maxSupportedTexAnisoTaps = 0;
 
 	void keyPressEvent( QKeyEvent * event );
 	void keyReleaseEvent( QKeyEvent * event );