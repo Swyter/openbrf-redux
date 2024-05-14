Swyter/openbrf-redux

commit 3e6e4fdfb7be1f58b510744cc59abfdd3ff0cc0e

glwidgets: Fix OpenBRF switching to female versions of armour mesh when playing skeletal animations.
Don't play back the vertex frame animation (used for armor feminization) when this is a skinned mesh simultaneously playing a skeletal animation; otherwise we'd always end up showing it with the (last vertex frame) female variant. Reported by @Earendil.

glwidgets.cpp
@@ -515,7 +515,7 @@ void GLWidget::renderBrfItem(const BrfTexture& t){
 
 void GLWidget::renderBrfItem (const BrfMesh& p){
 	float fi = 0;
-	if (p.HasVertexAni()) {
+	if (p.HasVertexAni() && !p.IsSkinned()) { /* swy: don't play back the vertex frame animation (used for armor feminization) when this is a skinned mesh simultaneously playing a skeletal animation; otherwise we'd always end up showing it with the (last vertex frame) female variant. Reported by @Earendil */
 		if (!skeletalAnimation() && runningState==PLAY) {
             fi = floatMod( relTime*runningSpeed, (int)p.frame.size()-3) +2;
 			if (fi<0) fi=0;

commit 0f812b00a05d8d6d0b61f5d207ae794dce14cdb2

glwidgets: Fix not being able to change vertex animation frames on skinned meshes when the skeletal animation is set to «no animation», the regression was introduced in 3e6e4fdfb7, female armor variants still play skeletal animations just fine. Reported by @Dalion.

glwidgets.cpp
@@ -515,8 +515,8 @@ void GLWidget::renderBrfItem(const BrfTexture& t){
 
 void GLWidget::renderBrfItem (const BrfMesh& p){
 	float fi = 0;
-	if (p.HasVertexAni() && !p.IsSkinned()) { /* swy: don't play back the vertex frame animation (used for armor feminization) when this is a skinned mesh simultaneously playing a skeletal animation; otherwise we'd always end up showing it with the (last vertex frame) female variant. Reported by @Earendil */
-		if (!skeletalAnimation() && runningState==PLAY) {
+	if (p.HasVertexAni()) {
+		if (/* swy: was !skeletalAnimation() */ !p.IsSkinned() && runningState==PLAY) {  /* swy: don't play back the vertex frame animation (used for armor feminization) when this is a skinned mesh simultaneously playing a skeletal animation; otherwise we'd always end up showing it with the (last vertex frame) female variant. Reported by @Earendil */
             fi = floatMod( relTime*runningSpeed, (int)p.frame.size()-3) +2;
 			if (fi<0) fi=0;
 			if (selFrameN != (int)fi) {