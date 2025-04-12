9816fa88281eff07e382c1955d11ff58a0f69ef7
Swyter <swyterzone@gmail.com>
2025-04-13 00:10:23 +0200

    guipanel: Fix a long-standing off-by-one bug in the Shader tab selection logic, where the first shader element (index 0) would show up in/update the contents of the panel info, staying blank or keeping any previous data from a previously selected entry.
                      It only worked from the second element (index 1) onwards. Reported by @Erundil in the TaleWorlds Forum thread. Thanks! :)
    
                       https://forums.taleworlds.com/index.php?threads/462517/page-4#post-9913824

--- guipanel.cpp
+++ guipanel.cpp
@@ -1039,7 +1039,7 @@ switch (TokenEnum(k)){
       {
       if (!newsel.size()) break;
       int sel = newsel[0].row();
-      if (sel<=0 || sel>=(int)data->shader.size())  break;
+      if (sel<0 || sel>=(int)data->shader.size())  break; /* swy: this was sel<=0 instead, there was a off-by-one bug, breaking the first element */
       BrfShader &s(data->shader[sel]);
       ui->leShaderTechnique->setText( s.technique );
       ui->leShaderFallback->setText( s.fallback );
